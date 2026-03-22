a582319e7d6be2bcd8455e7993e64577cfbb4fe6
Anna-Puchkova <annapuchkova1993@gmail.com>
2026-03-22 15:31:56 +0000

    Deploying to gh-pages from @ Anna-Puchkova/goit-js-hw-12@8219cc150da1a394e57bea96a3546b8112644a3b 🚀

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-/* empty css                      */(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();
+import{a as w,S as b,i as n}from"./assets/vendor-DQvd0HNi.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const a of e)if(a.type==="childList")for(const l of a.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&r(l)}).observe(document,{childList:!0,subtree:!0});function o(e){const a={};return e.integrity&&(a.integrity=e.integrity),e.referrerPolicy&&(a.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?a.credentials="include":e.crossOrigin==="anonymous"?a.credentials="omit":a.credentials="same-origin",a}function r(e){if(e.ep)return;e.ep=!0;const a=o(e);fetch(e.href,a)}})();const S="55034469-fe8c7c81350cc4405e2a2064a",d=15;async function p(t,s){const o={key:S,q:t,image_type:"photo",orientation:"horizontal",safesearch:!0,page:s,per_page:d};return(await w.get("https://pixabay.com/api/",{params:o})).data}const m=document.querySelector(".gallery"),y=new b(".gallery a",{captionsData:"alt",captionDelay:250});y.refresh();function h(t){const o=t.map(r=>`
+      <li class="gallery__item">
+        <a href="${r.largeImageURL}">
+          <img src="${r.webformatURL}" alt="${r.tags}" loading="lazy" />
+        </a>
+        <div class="info">
+  <p class="info-item">
+    <span class="info-title">Likes</span>
+    <span class="info-value">${r.likes}</span>
+  </p>
+  <p class="info-item">
+    <span class="info-title">Views</span>
+    <span class="info-value">${r.views}</span>
+  </p>
+  <p class="info-item">
+    <span class="info-title">Comments</span>
+    <span class="info-value">${r.comments}</span>
+  </p>
+  <p class="info-item">
+    <span class="info-title">Downloads</span>
+    <span class="info-value">${r.downloads}</span>
+  </p>
+</div>
+      </li>
+    `).join("");m.insertAdjacentHTML("beforeend",o),y.refresh()}function P(){m.innerHTML=""}function g(){document.querySelector(".loader").classList.add("active")}function L(){document.querySelector(".loader").classList.remove("active")}function v(){document.querySelector(".button-load-more").classList.add("is-hidden")}function q(){document.querySelector(".button-load-more").classList.remove("is-hidden")}const f=document.querySelector(".form"),_=document.querySelector(".button-load-more");let i=1,c="",u=0;f.addEventListener("submit",async t=>{if(t.preventDefault(),c=f.querySelector('input[name="search-text"]').value.trim(),!c){n.warning({message:"Please enter a search term!"});return}i=1,P(),v(),g();try{const o=await p(c,i);if(u=o.totalHits,o.hits.length===0){n.error({message:"Sorry, there are no images matching your search query. Please try again!"});return}h(o.hits),u>d&&q()}catch{n.error({message:"Something went wrong. Please try again later!"})}finally{L()}});_.addEventListener("click",async()=>{i+=1,g();try{const t=await p(c,i);h(t.hits);const s=Math.ceil(u/d);i>=s&&(v(),n.info({message:"We're sorry, but you've reached the end of search results."}));const r=document.querySelector(".gallery__item").getBoundingClientRect().height;window.scrollBy({top:r*2,behavior:"smooth"})}catch{n.error({message:"Something went wrong. Please try again later!"})}finally{L()}});
 //# sourceMappingURL=index.js.map
